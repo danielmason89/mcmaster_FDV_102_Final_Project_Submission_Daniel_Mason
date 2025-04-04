0d6b91e715e61e74f4d741a5895e8e6b5dc01ec8
Daniel Mason <danmasson0@gmail.com>
2025-04-04 14:09:26 -0400

    added CSS documentation, made mobile nav icon dynamic, and made some content improment updates

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 export default function myFunction() {
   const nav = document.getElementById("myTopnav");
-  if (nav.className === "topnav") {
-    nav.className += " responsive";
+  const menuIcon = document.querySelector('#menuToggleButton i');
+
+  if (nav.className === 'topnav') {
+    nav.className += ' responsive';
+
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-times');
   } else {
-    nav.className = "topnav";
+    nav.className = 'topnav';
+
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('menuToggleButton').addEventListener('click', myFunction);
+  document
+    .getElementById('menuToggleButton')
+    .addEventListener('click', myFunction);
 });
